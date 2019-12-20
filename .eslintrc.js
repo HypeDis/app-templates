33082bff0a5355a82881cae0f932423dd546aa6f
Mark <m.bae@live.com>
2019-12-19 19:40:04 -0800

    base config

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 6,
+  },
+  plugins: ['jest', 'prettier'],
+  extends: [
+    'standard',
+    'plugin:jest/recommended',
+    'plugin:prettier/recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+
+  rules: {
+    'consistent-return': 0,
+    'no-console': 0,
+    'no-var': 2,
+  },
+};
